5a9320edb4cd63abe53642358c9ea358b2e431b8
Corotea <gufvor@gmail.com>
2021-09-18 14:32:27 +0300

    lesson 4

--- src/Test2.java
+++ src/Test2.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Test2 {
     public static void main(String[] args) {
+        // Задание 2
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
 
